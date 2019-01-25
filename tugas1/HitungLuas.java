363ec81917bfb61d15ab5a94dde94a559a48298a
Tanty Malasari <tantymalasari@gmail.com>
2019-01-25 10:03:02 +0700

    menambah luas lingkaran

--- tugas1/HitungLuas.java
+++ tugas1/HitungLuas.java
@@ -7,6 +7,7 @@ public class HitungLuas {
     public static void main(String[] args) {
         //buatfunsgi yang namanya luasPersegiPanjang
         luasPersegiPanjang();
+        luasLingkaran();
     }
     
     //ini fungsi yg namanya sesuai yg di atas
@@ -34,7 +35,20 @@ public class HitungLuas {
         System.out.println("Luas persegi panjang " + luas);
         System.out.println("");
         
-        //jalankan deh biar cepet moveon
+    }
+    
+    public static void luasLingkaran() {
+        double jari;
+        double luas;
+        Scanner masukkan = new Scanner(System.in);
+        
+        System.out.print("Masukan jari-jari(cm) : ");
+        jari = masukkan.nextInt();
+
+        luas = 2 * jari * 3.14;
+
+        System.out.print("Luas lingkaran : " + luas);
+        System.out.println("");
     }
     
 }
