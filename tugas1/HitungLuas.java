a24c678142d46cd5cbc1fc9697df362083197c4a
Tanty Malasari <tantymalasari@gmail.com>
2019-01-25 09:53:46 +0700

    menambah luas persegi panjang

--- tugas1/HitungLuas.java
+++ tugas1/HitungLuas.java
@@ -1,9 +1,40 @@
 package tugas1;
 
+import java.util.Scanner;
+
 public class HitungLuas {
 
     public static void main(String[] args) {
+        //buatfunsgi yang namanya luasPersegiPanjang
+        luasPersegiPanjang();
+    }
+    
+    //ini fungsi yg namanya sesuai yg di atas
+    public static void luasPersegiPanjang() {
+        //deklarasi kenalin dulu variabelnya yg dibutuhkan
+        int panjang;
+        int lebar;
+        int luas;
         
+        //fungsi untuk menerima masukkan menggunakan scanner
+        Scanner masukkan = new Scanner(System.in);
+        
+        //buat form panjang lalu simpan k variabel panjang
+        System.out.print("Masukan panjang(cm) : ");
+        panjang = masukkan.nextInt();
+        
+        //buat form lebar lalu simpan k variabel lebar
+        System.out.print("Masukan Lebar(cm)   : ");
+        lebar = masukkan.nextInt();
+        
+        //ini rumusnya
+        luas = panjang * lebar;
+        
+        //keluarin hasilnya
+        System.out.println("Luas persegi panjang " + luas);
+        System.out.println("");
+        
+        //jalankan deh biar cepet moveon
     }
     
 }
